d82dd5352a353f91a00b021f7080676c3ebfe88e
Nihfkeol <nihfkeol@163.com>
2020-04-17 22:28:31 +0800

    拦截器，检查是否登录

--- springboot-restfulcrud/src/main/java/com/nihfkeol/config/MyMvcConfig.java
+++ springboot-restfulcrud/src/main/java/com/nihfkeol/config/MyMvcConfig.java
@@ -1,9 +1,11 @@
 package com.nihfkeol.config;
 
+import com.nihfkeol.component.LoginHandlerInterceptor;
 import com.nihfkeol.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,6 +26,15 @@ public class MyMvcConfig implements WebMvcConfigurer {
                 registry.addViewController("/").setViewName("index");
                 registry.addViewController("/index.html").setViewName("index");
                 registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //静态资源； *.css，*.js
+                //SpringBoot已经做好了静态资源映射
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/","/index.html","/user/login","/webjars/**");
             }
         };
         return adapter;
