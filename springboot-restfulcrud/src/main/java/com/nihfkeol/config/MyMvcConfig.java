c7b7caa6b441915ddddf7c0913616d6f3fe88a4c
Nihfkeol <nihfkeol@163.com>
2020-04-17 21:44:22 +0800

    登录功能实现

--- springboot-restfulcrud/src/main/java/com/nihfkeol/config/MyMvcConfig.java
+++ springboot-restfulcrud/src/main/java/com/nihfkeol/config/MyMvcConfig.java
@@ -23,6 +23,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("index");
                 registry.addViewController("/index.html").setViewName("index");
+                registry.addViewController("/main.html").setViewName("dashboard");
             }
         };
         return adapter;
---
+++ springboot-restfulcrud/src/main/java/com/nihfkeol/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.nihfkeol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @PostMapping("/user/login")
+    public String login(@RequestParam("username") String username,
+                         @RequestParam("password") String password,
+                         Map<String,Object> map){
+        if (!StringUtils.isEmpty(username) && password.equals("123456")) {
+            //防止表单重复提交，可以重定向到主页
+            return "redirect:/main.html";
+        }else {
+            map.put("msg","用户名密码错误");
+            return "index";
+        }
+    }
+
+}
