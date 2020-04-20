2418a4047050653b26b1c3d4ed4caa01fd22fa48
Nihfkeol <nihfkeol@163.com>
2020-04-20 16:02:32 +0800

    使用注解版，开启驼峰命名法

--- SpringBoot-MyBatis/src/main/java/com/nihfkeol/SpringbootMybatisApplication.java
+++ SpringBoot-MyBatis/src/main/java/com/nihfkeol/SpringbootMybatisApplication.java
@@ -1,8 +1,10 @@
 package com.nihfkeol;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.nihfkeol.mapper")
 @SpringBootApplication
 public class SpringbootMybatisApplication {
 
